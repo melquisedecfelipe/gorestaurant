d28f2c0a25fc748023ef9c7d6796d0491c466d7a
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-07-01 16:02:56 -0300

    feat: add products files

---
+++ backend/src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,64 @@
+import { getRepository, Repository, In } from 'typeorm';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
+
+class ProductsRepository implements IProductsRepository {
+  private ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = getRepository(Product);
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProductDTO): Promise<Product> {
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const findCustomer = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return findCustomer;
+  }
+
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const productsData = await this.ormRepository.find({
+      where: In(products.map(product => product.id)),
+    });
+
+    return productsData;
+  }
+
+  public async updateQuantity(
+    products: IUpdateProductsQuantityDTO[],
+  ): Promise<Product[]> {
+    const productsUpdated = this.ormRepository.create(products);
+
+    await this.ormRepository.save(productsUpdated);
+
+    return productsUpdated;
+  }
+}
+
+export default ProductsRepository;
